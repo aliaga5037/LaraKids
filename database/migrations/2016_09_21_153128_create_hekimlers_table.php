31cce589ede063a0bc56d301fbecc984fcc28fee
Leyla Abbasova <Leyla Abbasova>
2016-09-21 19:47:18 +0400

    database ok

---
+++ database/migrations/2016_09_21_153128_create_hekimlers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHekimlersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hekimlers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',255);
+            $table->string('surname',255);
+            $table->string('haqqinda',255);
+            $table->string('mail',255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('hekimlers');
+    }
+}
