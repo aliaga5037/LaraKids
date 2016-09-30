6a96e5b6d2e82adce57e9228ed35e21490282d92
Leyla Abbasova <Leyla Abbasova>
2016-09-30 17:03:56 +0400

    register+

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container"  >
+<div class="container" id="input_transparent">
     <div class="row" >
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-8 col-md-offset-2" >
             <div class="panel panel-default" >
-                <div class="panel-heading">Qeydiyyat</div>
+                <div class="panel-heading" style="color:white;">Qeydiyyat</div>
                 <div class="panel-body" >
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+                    <form class="form-horizontal" role="form"  method="POST" action="{{ url('/register') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Ad</label>
+                            <label for="name" class="col-md-4 control-label" style="color:white;">Ad</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}">
@@ -25,7 +25,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Adress</label>
+                            <label for="email" class="col-md-4 control-label" style="color:white;">E-Mail Adress</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
@@ -39,7 +39,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Şifrə</label>
+                            <label for="password" class="col-md-4 control-label" style="color:white;">Şifrə</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password">
@@ -53,7 +53,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Şifrəni təsdiqlə</label>
+                            <label for="password-confirm" class="col-md-4 control-label" style="color:white;">Şifrəni təsdiqlə</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
